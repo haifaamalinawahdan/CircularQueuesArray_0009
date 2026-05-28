352ce029d3daf071873db339c5390655742fa7ab
haifaamalinawahdan <haifanew3@gmail.com>
2026-05-28 15:12:14 +0700

    MEMBUAT METHOD PENGHAPUSAN DATA BERISIKAN IF CONDITION UNTUK MENGECEK ANTRIAN KOSONG

--- queque.cpp
+++ queque.cpp
@@ -44,4 +44,13 @@ public:
         queue_array[REAR] = num; 
     }
 
+    void remove()
+    {
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "\nQueue underflow\n";
+            return; 
+        }
+    }
 }
